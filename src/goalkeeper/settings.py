67522fe03cb3c2e71a19967733625051e5367ed4
Sven Seeberg <mail@sven-seeberg.de>
2019-07-30 16:40:37 +0200

    Add e-mail host settings

--- src/goalkeeper/settings.py
+++ src/goalkeeper/settings.py
@@ -122,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.example.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mail@exmple.com'
+EMAIL_HOST_PASSWORD = 'password'
