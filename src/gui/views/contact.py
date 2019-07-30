83deb82a862eccd20b79deeb3c86784bf3d53e60
Sven Seeberg <mail@sven-seeberg.de>
2019-07-30 19:50:04 +0200

    Fixes to contact

--- src/gui/views/contact.py
+++ src/gui/views/contact.py
@@ -4,7 +4,7 @@ Contact form for users
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.core.mail import EmailMessage
-
+from django.http import HttpResponseRedirect
 
 @login_required
 def contact_form(request):
@@ -20,6 +20,8 @@ def send_contact_form(request):
     """
     Send contact form via e-mail to admin team.
     """
+    if 'message' not in request.POST:
+        return HttpResponseRedirect("/gui/contact/")
     email = EmailMessage('ISH Goalkeeper - Benachrichtigung', request.POST['message'], reply_to=[request.user.email], from_email='bunterball@geeq.de', to=['seeberg@integreat-app.de'])
     email.send()
     return render(request, 'contact.html', context={"sent": True})
