743b456b99940227b4374b877627f3ef1d80c3ab
Sven Seeberg <mail@sven-seeberg.de>
2019-08-02 11:38:45 +0200

    Updates to URLs and preprocessor

--- src/gui/admin.py
+++ src/gui/admin.py
@@ -1,17 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Course, CourseUsers, CourseLesson, News, Profile, Lesson, FeedbackLesson, CourseLessonFeedback, LessonMeta, LessonMetaData, File, Attachment
+from .models import Course, CourseUser, CourseLesson, News, Profile, Lesson, Feedback, LessonMeta, LessonMetaData, Attachment
 
 admin.site.register(Course)
 admin.site.register(CourseLesson)
 admin.site.register(News)
 admin.site.register(Profile)
 admin.site.register(Lesson)
-admin.site.register(FeedbackLesson)
 admin.site.register(LessonMeta)
 admin.site.register(LessonMetaData)
-admin.site.register(CourseLessonFeedback)
-admin.site.register(CourseUsers)
-admin.site.register(File)
+admin.site.register(Feedback)
+admin.site.register(CourseUser)
 admin.site.register(Attachment)
--- src/gui/urls.py
+++ src/gui/urls.py
@@ -1,6 +1,10 @@
+"""
+URL routing
+"""
 from django.urls import path
 from . import views
 
+# pylint: disable=C0103
 urlpatterns = [
     path('', views.login_screen, name="login_screen"),
     path('login/', views.login_user, name='login_user'),
@@ -10,4 +14,11 @@ urlpatterns = [
     path('contact/', views.contact_form, name="contact_form"),
     path('send_contact/', views.send_contact_form, name="send_contact_form"),
     path('profile/', views.edit_profile, name="edit_profile"),
+    path('courses/', views.show_courses, name="show_courses"),
+    path('course/<int:course_id>/',
+         views.course_details,
+         name="course_details"),
+    path('course/<int:course_id>/<int:lesson_id>/',
+         views.course_lesson,
+         name="course_lesson"),
 ]
