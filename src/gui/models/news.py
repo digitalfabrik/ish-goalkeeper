3d985a333b5625a4da66943b9214bc36a74fd163
Sven Seeberg <mail@sven-seeberg.de>
2019-08-21 12:30:00 +0200

    change news, automatic date, author, fixes #31

--- src/gui/models/news.py
+++ src/gui/models/news.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class News(models.Model):
     title = models.CharField('Titel', max_length=200, blank=False)
     text = models.TextField('Nachricht', blank=False)
-    pub_date = models.DateTimeField('Veröffentlichungsdatum')
+    pub_date = models.DateTimeField('Veröffentlichungsdatum', auto_now_add=True)
+    author = models.ForeignKey(User,
+                               null=True, blank=True,
+                               on_delete=models.SET_NULL)
 
     def __unicode__(self):
         return str(self.title)
